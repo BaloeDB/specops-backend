e5167545f898f05bb46a0857e26e13e93a083fc7
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-03-13 12:10:20 +0100

    Small change to trigger redeploy

--- src/main/java/nl/itvitae/specops/SpecopsApplication.java
+++ src/main/java/nl/itvitae/specops/SpecopsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpecopsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpecopsApplication.class, args);
 	}
 
